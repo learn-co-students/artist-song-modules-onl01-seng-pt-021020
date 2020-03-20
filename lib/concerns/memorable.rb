17522d3fdb5d80720defd347718b2f5d548558bb
drewsview34 <drewsview34@gmail.com>
2020-03-19 20:56:53 -0700

    finished lab

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,17 @@
-module memorable
-    def self.reset_all
-        self.all.clear
+module Memorable
+  module ClassMethods
+    def reset_all
+      all.clear
     end
     
-      def self.count
-        self.all.count
-      end
+    def count
+      all.count
+    end
+  end
+  
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,27 @@
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/paramable'
+
 require 'pry'
 
-class Song
+class Song  
+  
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  # def initialize
+  #   @@songs << self
+  # end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
@@ -30,7 +39,7 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
